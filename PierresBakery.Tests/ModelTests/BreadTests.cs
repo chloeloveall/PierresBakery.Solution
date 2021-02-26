623b600809869fc4e8a77a74e1ac64905a285784
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-26 10:23:08 -0800

    Pass GetSingleLoafPrice test. Add argument to BreadConstructor Test

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -10,8 +10,17 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetSingleLoafPrice_ReturnsSingleLoafPrice_Int()
+    {
+      int loaf = 5;
+      Bread newBread = new Bread(loaf);
+      int loafPriceResult = newBread.Loaf;
+      Assert.AreEqual(loaf, loafPriceResult);
     }
 
   }
