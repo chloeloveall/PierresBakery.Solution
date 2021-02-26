77aa519199a542ce176e6f83988dc2ff78f829a0
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-26 09:12:34 -0800

    Add using statements, namespace, and public test class

---
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Test methods go here
+  }
+}
