3c8aac371e68f2c4f446de1443f0121fd17ce47c
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-26 09:53:01 -0800

    Pass test for Bread constructor

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PierresBakery;
+using PierresBakery.Models;
 
 namespace PierresBakery.Tests
 {
   [TestClass]
   public class BreadTests
   {
-    // Test methods go here
+    
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
   }
 }
