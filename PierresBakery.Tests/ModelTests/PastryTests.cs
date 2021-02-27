3a6b75ff172c52dae216ea30f0deb1e2354ee756
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-27 15:48:24 -0800

    Pass test of pastry constructor instance

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -1,11 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
+using System.Collections.Generic;
 
 namespace PierresBakery.Tests
 {
   [TestClass]
   public class PastryTests
   {
-    // Test methods go here
+
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
   }
 }
